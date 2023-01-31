76d62b0a3dc22573fb432733dabf2b933bc11887
torla <munford.ns@gmail.com>
2023-01-31 11:25:21 +0100

    OOP exercises

---
+++ python_exercises/OOP/examples/01_OOP.py
@@ -0,0 +1,93 @@
+import random
+
+
+class Prova:  # definition
+    statico = 'statico'  # static name
+
+    def __init__(self, x) -> None:  # constructor
+        super().__init__()
+        self.x = x  # attribute x
+        self.y = self.x * 2
+
+    def method(self):  # method
+        return self.x + self.y
+
+
+print(Prova.statico)  # access static
+prova = Prova(1)  # Instantiation
+print(prova.method())  # calling a method
+print(prova.y)  # accessing attributes
+
+print(prova.__dict__)  # access underling dict
+
+
+class Polygon:  # abstract class, only used as base for other class
+
+    def __init__(self) -> None:
+        pass
+
+    def area(self):  # abstract method to be overridden
+        pass
+
+    def __eq__(self, o: object) -> bool:  # define the == operator to be overridden
+        return isinstance(o, Polygon)
+
+
+class Rectangle(Polygon):  # class derived from polygon
+    def __init__(self, side_a_len: float, side_b_len: float) -> None:
+        super().__init__()  # always call the super constructor
+        self.side_a_len = side_a_len
+        self.side_b_len = side_b_len
+
+    def area(self) -> float:  # override the base area
+        return self.side_a_len * self.side_b_len
+
+    def __eq__(self, o: object) -> bool:  # check if instance rectangle and then check every attribute
+        return super().__eq__(o) and isinstance(o, Rectangle) \
+               and self.side_b_len == o.side_b_len \
+               and self.side_a_len == o.side_a_len
+
+
+class Square(Rectangle):
+    def __init__(self, side_len: float) -> None:
+        super().__init__(side_len, side_len)  # a square is a rectangle with same length side
+
+    def __eq__(self, o: object) -> bool:
+        return super().__eq__(o) and isinstance(o, Square)
+
+
+class Triangle(Polygon):
+
+    def __init__(self, base: float, high: float) -> None:
+        super().__init__()
+        self.base = base
+        self.high = high
+
+    def area(self) -> float:  # alternative implementation of area() for the triangle
+        return (self.base * self.high) / 2
+
+    def __eq__(self, o: object) -> bool:
+        return super().__eq__(o) \
+               and isinstance(o, Triangle) \
+               and self.base == o.base \
+               and self.high == o.high
+
+
+print("\n\n\n")
+t = Triangle(6, 8)
+s = Square(5)
+print(t == s)  # using the operator == auto call __eq__
+print(s == Square(5))
+# create list of polygon selecting randomly the type
+l: [Polygon] = []
+for i in range(0, 10):
+    r = random.randint(0, 3)
+    if r == 0:
+        l.append(Rectangle(3, 4))  # area  = 12
+    elif r == 1:
+        l.append(Square(2))  # area  = 4
+    else:
+        l.append(Triangle(1, 3))  # area  = 1.5
+# the right area method is called
+for p in l:
+    print(p.area())
---
+++ python_exercises/OOP/exercises/01_OOP.py
@@ -0,0 +1,106 @@
+# 1 - Create a class rapresenting a point in 2d space implementing:
+#               .__eq__
+#               distance(self,p:point)-> float that return the distance
+# 2 - Using the point class create a class that represent a broken line, implementing __eq__ and a .length()->float
+# 3 - Using the broken line class as base, create a class representing a closed broken line
+# 4 - Using the closed broken line as base create a Square class, implementing the __area__ method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SOLUTIONS
+# 1
+import math
+
+
+class Point:
+
+    def __init__(self, x: float, y: float) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+
+    def distance(self, p) -> float:
+        assert (isinstance(p, Point))
+        return math.sqrt((self.x - p.x) ** 2 + (self.y - p.y) ** 2)
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Point) and self.x == o.x and self.y == o.y
+
+
+# 2
+class BrokenLine:
+    def __init__(self, points: [Point]) -> None:
+        super().__init__()
+        self.points: [Point] = points
+
+    def length(self) -> float:
+        return sum([self.points[i].distance(self.points[i + 1]) for i in range(0, len(self.points) - 1)])
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, BrokenLine) \
+               and len(self.points) == len(o.points) \
+               and False not in [self.points[i] == o.points for i in range(0, len(self.points))]
+
+
+# 3
+class ClosedBrokenLine(BrokenLine):
+    def __init__(self, points: [Point]) -> None:
+        super().__init__(points)
+
+    def length(self) -> float:
+        return super().length() + self.points[0].distance(self.points[-1])
+
+
+# 4
+class Square(ClosedBrokenLine):
+    def __init__(self, point_a, point_b, point_c) -> None:
+        assert (point_a.distance(point_b) == point_b.distance(point_c))
+        assert (abs(point_a.x - point_b.x) == abs(point_b.y - point_c.y)
+                and abs(point_a.y - point_b.y) == abs(point_b.x - point_c.x))
+        points = [point_a, point_b, point_c,
+                  Point(x=point_c.x + (point_a.x - point_b.x), y=point_c.y + (point_a.y - point_b.y))]
+
+        super().__init__(points)
+
+    def __eq__(self, o: object) -> bool:
+        return super().__eq__(o) and isinstance(o, Square)
+
+    def perimeter(self) -> float:
+        return self.length()
+
+    def area(self) -> float:
+        return self.points[0].distance(self.points[1]) ** 2
+
+
+s = Square(Point(0, 1), Point(0, 0), Point(1, 0))
+print(s.area())
+print(s.perimeter())
